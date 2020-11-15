83d0e7c1004d9afe94a1db5dd14baae2a802ac6a
812jay <yongje812@gmail.com>
2020-11-15 21:47:10 +0900

    #5.1 Connect and Disconnect Notifications

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,7 @@
 import { initSockets } from "./sockets";
 
 const body = document.querySelector("body");
-const loginForm = document.getElementById("jsLogin")
+const loginForm = document.getElementById("jsLogin");
 
 const NICKNAME = "nickname";
 const LOGGED_OUT = "loggedOut";
@@ -10,19 +10,19 @@ const LOGGED_IN = "loggedIn";
 const nickname = localStorage.getItem(NICKNAME);
 
 const logIn = (nickname) => {
-    const socket = io("/");
-    socket.emit(window.events.setNickname, { nickname });
-    initSockets(socket);
-}
+  const socket = io("/");
+  socket.emit(window.events.setNickname, { nickname });
+  initSockets(socket);
+};
 
-if(nickname === null){
+if (nickname === null) {
   body.className = LOGGED_OUT;
 } else {
   body.className = LOGGED_IN;
   logIn(nickname);
 }
 
-const handleFormSubmit = (e) =>{
+const handleFormSubmit = (e) => {
   e.preventDefault();
   const input = loginForm.querySelector("input");
   const { value } = input;
@@ -30,8 +30,8 @@ const handleFormSubmit = (e) =>{
   localStorage.setItem(NICKNAME, value);
   body.className = LOGGED_IN;
   logIn(value);
-}
+};
 
-if(loginForm){
+if (loginForm) {
   loginForm.addEventListener("submit", handleFormSubmit);
 }
